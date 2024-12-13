46ca5617638aff46e72922df910a1c2513d8052f
ThaoPham372 <tuongthao02101992@gmail.com>
2024-12-14 00:15:33 +0700

    Update function edit & search

--- MenuStudent.java
+++ MenuStudent.java
@@ -6,6 +6,12 @@ public class MenuStudent {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         List<Student> studentList = new ArrayList<Student>();
+//        Student student1 = new Student("Thao", 12, "abc");
+//        Student student2 = new Student("Quang", 14, "def");
+//        Student student3 = new Student("Anh Nam", 18, "ert");
+//        studentList.add(student1);
+//        studentList.add(student2);
+//        studentList.add(student3);
         int choice;
     do {
         System.out.println("-----Student Management Menu-----");
@@ -26,7 +32,7 @@ public class MenuStudent {
                 break;
             case 3:
                 System.out.println("Please Edit Student!");
-                System.out.println("Enter Student Name to Edit: ");
+                System.out.print("Enter Student Name to Edit: ");
                 String oldName = sc.nextLine();
                 editStudent(oldName, studentList);
 
@@ -48,10 +54,14 @@ public class MenuStudent {
 
     }
 
+    /**
+     * Edit Student by Name
+     *
+     * @param oldName String
+     * @param studentList List<Student>
+     */
     public static void editStudent (String oldName, List <Student> studentList) {
         Scanner sc = new Scanner(System.in);
-
-
         boolean isFound = false;
         for (Student student : studentList) {
             if (student.getName().equals(oldName)) {
@@ -70,9 +80,14 @@ public class MenuStudent {
         }
     }
 
+    /**
+     * Search Student by Name
+     *
+     * @param studentList List<Student>
+     */
     public static void searchStudentByName(List<Student> studentList){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Student Name to Search: ");
+        System.out.print("Enter Student Name to Search: ");
         String name = sc.nextLine();
 
         boolean isFound = false;
