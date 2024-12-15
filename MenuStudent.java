48537885b54c5fe616748a71643de6d76acfb6bc
ThaoPham372 <tuongthao02101992@gmail.com>
2024-12-15 13:21:21 +0700

    Fix new comment

--- MenuStudent.java
+++ MenuStudent.java
@@ -13,47 +13,44 @@ public class MenuStudent {
 //        studentList.add(student2);
 //        studentList.add(student3);
         int choice;
-    do {
-        System.out.println("-----Student Management Menu-----");
-        System.out.println("1. Add Student");
-        System.out.println("2. Delete Student");
-        System.out.println("3. Edit Student");
-        System.out.println("4. Search Student By Name");
-        System.out.println("5. Exit");
-        System.out.println("Enter your choice: ");
-        choice = sc.nextInt();
-        sc.nextLine();
-        switch (choice) {
-            case 1:
-                System.out.println("Please Add Student!");
-                break;
-            case 2:
-                System.out.println("Please Delete Student!");
-                break;
-            case 3:
-                System.out.println("Please Edit Student!");
-                System.out.print("Enter Student Name to Edit: ");
-                String oldName = sc.nextLine();
-                editStudent(oldName, studentList);
+        do {
+            System.out.println("-----Student Management Menu-----");
+            System.out.println("1. Add Student");
+            System.out.println("2. Delete Student");
+            System.out.println("3. Edit Student");
+            System.out.println("4. Search Student By Name");
+            System.out.println("5. Exit");
+            System.out.println("Enter your choice: ");
+            choice = sc.nextInt();
+            sc.nextLine();
 
-                break;
-            case 4:
-                System.out.println("Please Search Student By Name!");
-                searchStudentByName(studentList);
-                break;
-            case 5:
-                System.out.println("Exit Program");
-                break;
-            default:
-                System.out.println("Invalid choice");
-                break;
-        }
+            switch (choice) {
+                case 1:
+                    System.out.println("Please Add Student!");
+                    break;
+                case 2:
+                    System.out.println("Please Delete Student!");
+                    break;
+                case 3:
+                    System.out.println("Please Edit Student!");
+                    System.out.print("Enter Student Name to Edit: ");
+                    String oldName = sc.nextLine();
+                    editStudent(oldName, studentList);
+                    break;
+                case 4:
+                    System.out.println("Please Search Student By Name!");
+                    searchStudentByName(studentList);
+                    break;
+                case 5:
+                    System.out.println("Exit Program");
+                    break;
+                default:
+                    System.out.println("Invalid choice");
+                    break;
+            }
 
-    } while (choice != 5);
-
-
+        } while (choice != 5);
     }
-
     /**
      * Edit Student by Name
      *
@@ -103,9 +100,7 @@ public class MenuStudent {
             System.out.println("Student Not Found");
         }
     }
-
-
-    }
+}
 
 
 
